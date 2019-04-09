e3567673950b9a3a10b97fffaee2d4bf4e8d3f1c
abdullah alammar <s201449260@kfupm.edu.sa>
2019-04-09 21:28:43 +0300

    fix bugs

--- Image/ViewController.swift
+++ Image/ViewController.swift
@@ -110,6 +110,8 @@ UINavigationControllerDelegate, UITextFieldDelegate{
         super.viewWillAppear(animated)
         
         cameraBtn.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyBoardNotifications()
+
         
         
     }
@@ -128,17 +130,26 @@ UINavigationControllerDelegate, UITextFieldDelegate{
     
     func unsubscribeFromKeyboardNotifications(){
         
-        NotificationCenter.default.removeObserver(self,  name: UIResponder.keyboardWillShowNotification, object: nil)
+//        NotificationCenter.default.removeObserver(self,  name: UIResponder.keyboardDidHideNotification, object: nil)
+         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
     
     @objc func keyboardWillShow(_ notification : Notification) {
-        view.frame.origin.y -= getKeyboardHeight(notification)
+        if   view.frame.origin.y == 0 {
+             view.frame.origin.y -= getKeyboardHeight(notification)
+            
+        }
+       
     }
     
     
     @objc func keyboardWillHide(_ notification : Notification) {
-        view.frame.origin.y = 0.0
+        if view.frame.origin.y != 0{
+             view.frame.origin.y = 0
+        }
+       
     }
     
     
